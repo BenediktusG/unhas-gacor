3b11e7b1435c7fb8367de5cc722cea2149ecb321
von-ai <kuhhakuzuhdi@gmail.com>
2025-08-07 22:54:29 +0800

    fix regis

--- src/application/web.js
+++ src/application/web.js
@@ -6,15 +6,20 @@ import cors from 'cors';
 dotenv.config();
 export const web = express();
 
-web.use(cors({
+web.use(
+  cors({
     origin: process.env.FRONT_END_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-}));
-
-web.use(userRouter);
+  })
+);
 
 web.use(express.json());
+web.use(userRouter);
 
 web.listen(process.env.APP_PORT, () => {
-    console.log(`Application is running in http://localhost:${process.env.APP_PORT}`);
+  console.log(
+    `Application is running in http://localhost:${process.env.APP_PORT}`
+  );
 });
--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,60 +1,64 @@
-import { db } from "../application/database.js";
-import { AuthorizationError } from "../error/authorizationError.js";
-import { ConflictError } from "../error/conflict-error.js";
-import { registerUserValidation } from "../validation/user-validation.js";
-import { validate } from "../validation/validation.js";
+import { db } from '../application/database.js';
+import { AuthorizationError } from '../error/authorizationError.js';
+import { ConflictError } from '../error/conflict-error.js';
+import { registerUserValidation } from '../validation/user-validation.js';
+import { validate } from '../validation/validation.js';
 
-const getMoney = async(user) => {
-    const { money } = await db.collection('user-profile').doc(user.uid).get();
-    return {
-        money: money,
-    };
+const getMoney = async (user) => {
+  const { money } = await db.collection('user-profile').doc(user.uid).get();
+  return {
+    money: money,
+  };
 };
 
 const checkBonusAvailability = async (user) => {
-    const { money } = await db.collection('user-profile').doc(user.uid).get();
-    if (money < 100.000) {
-        return {
-            eligible: true,
-        };
-    } else {
-        return {
-            eligible: false,
-        };
-    }
+  const { money } = await db.collection('user-profile').doc(user.uid).get();
+  if (money < 100.0) {
+    return {
+      eligible: true,
+    };
+  } else {
+    return {
+      eligible: false,
+    };
+  }
 };
 
 const claimBonus = async (user) => {
-    const { money } = await db.collection('user-profile').doc(user.uid).get();
-    if (money > 100000) {
-        throw new AuthorizationError('Access denied', 'UNAUTHORIZED_ACTION');
-    }
-    await db.collection("money").doc(user.uid).set(money + 100000);
-    return {
-        bonus: 100000,
-        money: money + 100000,
-    };
+  const { money } = await db.collection('user-profile').doc(user.uid).get();
+  if (money > 100000) {
+    throw new AuthorizationError('Access denied', 'UNAUTHORIZED_ACTION');
+  }
+  await db
+    .collection('money')
+    .doc(user.uid)
+    .set(money + 100000);
+  return {
+    bonus: 100000,
+    money: money + 100000,
+  };
 };
 
 const registerProfile = async (request, user) => {
-    const { fullName } = validate(registerUserValidation, request);
-    await db.collection('user-profile').doc(user.uid).set({
-        fullName: fullName,
-        money: 0,
-    });
+  console.log(request);
+  const { fullName } = validate(registerUserValidation, request);
+  await db.collection('user-profile').doc(user.uid).set({
+    fullName: fullName,
+    money: 0,
+  });
 };
 
 const getProfile = async (user) => {
-    const profile = await db.collection('user-profile').doc(user.uid).get();
-    if (!profile) {
-        throw new ConflictError('missing user information', 'INCOMPLETE_PROFILE');
-    }
+  const profile = await db.collection('user-profile').doc(user.uid).get();
+  if (!profile) {
+    throw new ConflictError('missing user information', 'INCOMPLETE_PROFILE');
+  }
 };
 
 export default {
-    getMoney,
-    checkBonusAvailability,
-    claimBonus,
-    registerProfile,
-    getProfile,
+  getMoney,
+  checkBonusAvailability,
+  claimBonus,
+  registerProfile,
+  getProfile,
 };
