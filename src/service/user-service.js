28f9a2b4123bffd23275917448b3f4fee5306845
BenediktusG <benediktus.gianto.jarod@gmail.com>
2025-08-08 14:46:38 +0800

    fix(user): return actual document fields by adding .data() after .get()

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -5,14 +5,14 @@ import { registerUserValidation } from '../validation/user-validation.js';
 import { validate } from '../validation/validation.js';
 
 const getMoney = async (user) => {
-  const { money } = await db.collection('user-profile').doc(user.uid).get();
+  const { money } = await db.collection('user-profile').doc(user.uid).get().data();
   return {
     money: money,
   };
 };
 
 const checkBonusAvailability = async (user) => {
-  const { money } = await db.collection('user-profile').doc(user.uid).get();
+  const { money } = (await db.collection('user-profile').doc(user.uid).get()).data();
   if (money < 100.0) {
     return {
       eligible: true,
@@ -25,7 +25,7 @@ const checkBonusAvailability = async (user) => {
 };
 
 const claimBonus = async (user) => {
-  const { money } = await db.collection('user-profile').doc(user.uid).get();
+  const { money } = (await db.collection('user-profile').doc(user.uid).get()).data();
   if (money > 100000) {
     throw new AuthorizationError('Access denied', 'UNAUTHORIZED_ACTION');
   }
@@ -49,10 +49,11 @@ const registerProfile = async (request, user) => {
 };
 
 const getProfile = async (user) => {
-  const profile = await db.collection('user-profile').doc(user.uid).get();
+  const profile = (await db.collection('user-profile').doc(user.uid).get()).data();
   if (!profile) {
     throw new ConflictError('missing user information', 'INCOMPLETE_PROFILE');
   }
+  return profile;
 };
 
 export default {
